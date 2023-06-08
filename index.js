5f15e013c8a5204c1bf3f08d984c6242c7c5eb60
Beaglez <78619059+TheBeaglezBest@users.noreply.github.com>
2023-06-08 16:29:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-function unblock() {
-  var url = document.getElementById("url").value;
-  document.getElementById("proxy").src = "https://cors-anywhere.herokuapp.com/" + url;
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const apiUrl = 'https://api.example.com/';
+
+function calculate() {
+  const expression = document.querySelector('.display-screen').textContent;
+  const result = eval(expression);
+  document.querySelector('.display-screen').textContent = result;
+  
+  fetch(proxyUrl + apiUrl, {
+    method: 'POST',
+    body: JSON.stringify({ expression, result }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 }
+
+document.querySelectorAll('.number, .operator').forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.textContent;
+    document.querySelector('.display-screen').textContent += value;
+  });
+});
+
+document.querySelector('.clear').addEventListener('click', () => {
+  document.querySelector('.display-screen').textContent = '';
+});
